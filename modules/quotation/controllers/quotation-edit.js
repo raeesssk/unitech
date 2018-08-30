8270cec16ae35da43ef4354a67cdad93f8f9868b
clayton dsouza <dsouzaclayton95@gmail.com>
2018-08-30 20:34:46 +0530

    Quotation Edit

--- modules/quotation/controllers/quotation-edit.js
+++ modules/quotation/controllers/quotation-edit.js
@@ -5,10 +5,6 @@ angular.module('quotation').controller('quotationEditCtrl', function ($rootScope
   $scope.quotationId = $routeParams.quotationId;
   $scope.apiURL = $rootScope.baseURL+'/quotation/edit/'+$scope.quotationId;
   $scope.personalDetails = []; 
-  $scope.oldProductDetails=[];
-  $scope.oldMachineDetails=[];
-  $scope.removeProductDetails=[];
-  $scope.removeMachineDetails=[];
   
   $scope.getQuotation = function () {
       $http({
@@ -21,31 +17,9 @@ angular.module('quotation').controller('quotationEditCtrl', function ($rootScope
       {   
             quotationObj.forEach(function(value,key){
                   value.qm_date=$filter('date')(value.qm_date, "mediumDate");
-                   $http({
-                        method: 'GET',
-                        url: $rootScope.baseURL+'/customer/'+value.cm_id,
-                        headers: {'Content-Type': 'application/json',
-                                'Authorization' :'Bearer '+localStorage.getItem("unitech_admin_access_token")}
-                  })
-                  .success(function(custObj)
-                  {
-                        custObj.forEach(function(value1,key){
-                          value.old_qm_cm_id = value1;
-                          value.qm_cm = value1; 
-                        });                      
-                    $scope.quotation=value;
-                  })
-                  .error(function(data) 
-                  {   
-                      var dialog = bootbox.dialog({
-                        message: '<p class="text-center">Oops, Something Went Wrong! Please Refresh the Page.</p>',
-                          closeButton: false
-                      });
-                      setTimeout(function(){
-                          dialog.modal('hide'); 
-                      }, 1500);            
-                  });
 
+                  $scope.quotation=value;
+                  
                   $http({
                         method: 'GET',
                         url: $rootScope.baseURL+'/quotation/details/'+$scope.quotationId,
@@ -55,7 +29,32 @@ angular.module('quotation').controller('quotationEditCtrl', function ($rootScope
                   .success(function(quotationObj)
                   {
                         quotationObj.forEach(function (value, key) {
-                          $scope.oldProductDetails.push(value);
+                          value.machineDetails=[];
+                          value.newMachineDetails=[];
+                          value.removeMachineDetails=[];
+                            $http({
+                                  method: 'GET',
+                                  url: $rootScope.baseURL+'/quotation/details/machine/'+value.qpm_id,
+                                  headers: {'Content-Type': 'application/json',
+                                          'Authorization' :'Bearer '+localStorage.getItem("unitech_admin_access_token")}
+                            })
+                            .success(function(machineObj)
+                            { 
+                                  machineObj.forEach(function (value1, key1) {
+                                    value.machineDetails.push(value1);
+                                  });
+                            })
+                            .error(function(data) 
+                                  {   
+                                    var dialog = bootbox.dialog({
+                                      message: '<p class="text-center">Oops, Something Went Wrong! Please Refresh the Page.</p>',
+                                          closeButton: false
+                                      });
+                                      setTimeout(function(){
+                                          dialog.modal('hide'); 
+                                      }, 1500);            
+                            });
+                          $scope.personalDetails.push(value);
                         });     
                   })
                   .error(function(data) 
@@ -69,31 +68,7 @@ angular.module('quotation').controller('quotationEditCtrl', function ($rootScope
                         }, 1500); 
                   });
 
-                  $http({
-                        method: 'GET',
-                        url: $rootScope.baseURL+'/quotation/details/machine/'+$scope.quotationId,
-                        headers: {'Content-Type': 'application/json',
-                                'Authorization' :'Bearer '+localStorage.getItem("unitech_admin_access_token")}
-                  })
-                  .success(function(machineObj)
-                  { 
-                        machineObj.forEach(function (value, key) {
-                          value.mm_search=value.mm_name+" "+value.mm_price;
-                          value.qpmm_total_cost= parseFloat(parseFloat(value.mm_price) * parseFloat(value.qpmm_mm_hr));
-                          $scope.quotation.qm_total_cost=$scope.quotation.qm_total_cost + value.qpmm_total_cost;
-                          $scope.oldMachineDetails.push(value);
-                        });
-                  })
-                  .error(function(data) 
-                        {   
-                          var dialog = bootbox.dialog({
-                            message: '<p class="text-center">Oops, Something Went Wrong! Please Refresh the Page.</p>',
-                                closeButton: false
-                            });
-                            setTimeout(function(){
-                                dialog.modal('hide'); 
-                            }, 1500);            
-                  });
+                  
 
             });
       })
@@ -116,75 +91,39 @@ angular.module('quotation').controller('quotationEditCtrl', function ($rootScope
         var nameRegex = /^\d+$/;
         var emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         
-        if($('#qm_design_no').val() == undefined || $('#qm_design_no').val() == ""){
+       if($('#qm_ref').val() == undefined || $('#qm_ref').val() == ""){
               var dialog = bootbox.dialog({
-                message: "<p class='text-center'>Please Enter Design Number!</p>",
-                    closeButton: false
-                }); 
-                dialog.find('.modal-body').addClass("btn-danger");
-                setTimeout(function(){
-                    dialog.modal('hide'); 
-                $('#qm_design_no').focus();
-                }, 1500);
-            }
-            else if($('#qm_quotation_no').val() == undefined || $('#qm_quotation_no').val() == ""){
-              var dialog = bootbox.dialog({
-                message: '<p class="text-center">Please Enter Quotation Number!</p>',
-                    closeButton: false
-                });
-                dialog.find('.modal-body').addClass("btn-danger");
-                setTimeout(function(){
-                    dialog.modal('hide');
-                    $('#qm_quotation_no').focus(); 
-                }, 1500);
-            }
-            else if($('#qm_cm').val() == undefined || $('#qm_cm').val() == ""){
-              var dialog = bootbox.dialog({
-                  message: "<p class='text-center'>Please Enter Customer's Name!</p>",
-                      closeButton: false
-                  });
-                  dialog.find('.modal-body').addClass("btn-danger");
-                  setTimeout(function(){
-                      dialog.modal('hide');
-                      $('#qm_cm_id').focus();  
-                  }, 1500);
-            }
-            else if($('#qm_date').val() == undefined || $('#qm_date').val() == ""){
-              var dialog = bootbox.dialog({
-                  message: '<p class="text-center">Please Enter The Date!</p>',
+                  message: '<p class="text-center">Please Enter The Reference!</p>',
                       closeButton: false
                   });
                   dialog.find('.modal-body').addClass("btn-danger");
                   setTimeout(function(){
                       dialog.modal('hide'); 
                       
-                      $('#qm_date').focus(); 
+                      $('#qm_ref').focus(); 
                   }, 1500);
             }
-            else if($('#qm_ref_no').val() == undefined || $('#qm_ref_no').val() == ""){
+            else if($('#qm_comment').val() == undefined || $('#qm_comment').val() == ""){
               var dialog = bootbox.dialog({
-                  message: '<p class="text-center">Please Enter The Reference Number!</p>',
+                  message: '<p class="text-center">Please Enter The Comment!</p>',
                       closeButton: false
                   });
                   dialog.find('.modal-body').addClass("btn-danger");
                   setTimeout(function(){
                       dialog.modal('hide'); 
                       
-                      $('#qm_ref_no').focus(); 
+                      $('#qm_comment').focus(); 
                   }, 1500);
             }
         else{
                 $('#btnsave').attr('disabled','true');
                 $('#btnsave').text("please wait...");
 
+                      $scope.quotation.qm_date = $('#qm_date').val();
+
                           $scope.obj={
                             quotation : $scope.quotation,
-                            machineDetails : $scope.machineDetails,
-                            personalDetails : $scope.personalDetails,
-                            oldProductDetails : $scope.oldProductDetails,
-                            oldMachineDetails : $scope.oldMachineDetails,
-                            removeProductDetails : $scope.removeProductDetails,
-                            removeMachineDetails : $scope.removeMachineDetails 
+                            purchaseMultipleData : $scope.personalDetails 
                           }
                           $http({
                             method: 'POST',
@@ -203,6 +142,7 @@ angular.module('quotation').controller('quotationEditCtrl', function ($rootScope
                                 setTimeout(function(){
                                     dialog.modal('hide'); 
                                 }, 1500);
+                                $scope.printDetails();
                                 $('#btnsave').text("Update");
                                 $('#btnsave').removeAttr('disabled');
                                window.location.href = '#/quotation';  
@@ -223,23 +163,6 @@ angular.module('quotation').controller('quotationEditCtrl', function ($rootScope
     };
 
 
-// Machine Details ADD/Remove Table
-    $scope.machineDetails = [];    
-      $scope.addmachine = function(machineDetails){
-
-          $scope.machineDetails.push({ 
-              'qpmm_mm_id': "",
-              'qpmm_mm_hr': "",
-              'qpmm_mm_price' : "",
-              'qpmm_total' : "",
-          });
-           $('#qpmm_mm_id').focus();
-      };
-    $scope.removeMachine = function(index){
-      $scope.machineDetails.splice(index,1);
-
-           $scope.calculate();
-    };
 
 
     $scope.addToCart = function(index){
@@ -273,7 +196,7 @@ angular.module('quotation').controller('quotationEditCtrl', function ($rootScope
               'qpmm_mm_id':$scope.personalDetails[index].qpmm_mm_id,
               'qpmm_mm_hr':$scope.personalDetails[index].qpmm_mm_hr
             }
-            $scope.personalDetails[index].machineDetails.push($scope.machineList);
+            $scope.personalDetails[index].newMachineDetails.push($scope.machineList);
             $scope.personalDetails[index].qpmm_mm_id = null;
             $scope.personalDetails[index].qpmm_mm_hr = 0;
             $scope.calculate($scope.personalDetails[index]);
@@ -282,97 +205,34 @@ angular.module('quotation').controller('quotationEditCtrl', function ($rootScope
     };
 
     $scope.removeItem = function(pindex,index){
+        $scope.personalDetails[pindex].newMachineDetails.splice(index,1);
+        $scope.calculate($scope.personalDetails[pindex]);
+        // $('#qpmm_mm_id').focus();
+    };
+
+    $scope.removeItemOld = function(pindex,index){
+        $scope.personalDetails[pindex].removeMachineDetails.push($scope.personalDetails[pindex].machineDetails[index]);
         $scope.personalDetails[pindex].machineDetails.splice(index,1);
         $scope.calculate($scope.personalDetails[pindex]);
         // $('#qpmm_mm_id').focus();
     };
 
     $scope.calculate=function(obj){
-      obj.dtm_total_cost=0;
+      obj.qpm_total_cost=0;
       $scope.quotation.qm_total_cost = 0;
       angular.forEach(obj.machineDetails, function(value,key){
 
+        obj.qpm_total_cost=obj.qpm_total_cost + value.qpmm_total_cost;
+      });
+      angular.forEach(obj.newMachineDetails, function(value,key){
+
       value.qpmm_total= parseFloat(parseFloat(value.qpmm_mm_id.mm_price) * parseFloat(value.qpmm_mm_hr));
-        obj.dtm_total_cost=obj.dtm_total_cost + value.qpmm_total;
+        obj.qpm_total_cost=obj.qpm_total_cost + value.qpmm_total;
       });
+
       angular.forEach($scope.personalDetails, function(value,key){
-        $scope.quotation.qm_total_cost=parseFloat($scope.quotation.qm_total_cost + value.dtm_total_cost);
+        $scope.quotation.qm_total_cost=parseFloat($scope.quotation.qm_total_cost + value.qpm_total_cost);
       });
-    };
-
-
-    $scope.checkAll = function () {
-        if (!$scope.selectedAll) {
-            $scope.selectedAll = true;
-        } else {
-            $scope.selectedAll = false;
-        }
-        angular.forEach($scope.machineDetails, function(machineDetail) {
-            machineDetail.selected = $scope.selectedAll;
-        });
-    };   
-    // END Machine Details ADD/Remove Table  
-
-
-    //design details on typeahead select
-    $scope.getDesignDetails=function(){
-        $scope.personalDetails=[];
-        $http({
-              method: 'GET',
-              url: $rootScope.baseURL+'/design/details/'+$scope.quotation.qm_design_no.dm_id,
-              headers: {'Content-Type': 'application/json',
-                      'Authorization' :'Bearer '+localStorage.getItem("unitech_admin_access_token")}
-            })
-            .success(function(design)
-            {     
-                  $scope.quotation.qm_design_no.dm_dely_date=$filter('date')($scope.quotation.qm_design_no.dm_dely_date, "mediumDate");
-                 design.forEach(function(value,key){
-                  value.machineDetails = [];
-                  value.qpmm_mm_hr = 0;
-                  value.dtm_total_cost = 0;
-                 $scope.personalDetails.push(value);
-
-                  });
-            })
-            .error(function(data) 
-            {   
-                var dialog = bootbox.dialog({
-                  message: '<p class="text-center">Oops, Something Went Wrong! Please Refresh the Page.</p>',
-                      closeButton: false
-                  });
-                  setTimeout(function(){
-                  $('#btnsave').text("Save");
-                  $('#btnsave').removeAttr('disabled');
-                      dialog.modal('hide'); 
-                  }, 1500);            
-            });
-    };
-
-    //design list record for Design Name input
-    $scope.getSearchDesign = function(vals) {
-      var searchTerms = {search: vals};
-        const httpOptions = {
-            headers: {
-              'Content-Type':  'application/json',
-              'Authorization': 'Bearer '+localStorage.getItem("unitech_admin_access_token")
-            }
-        };
-        return $http.post($rootScope.baseURL+'/quotation/typeahead/search', searchTerms, httpOptions).then((result) => {
-            return result.data;
-        });
-    };
-    //customer list record for Customer Name input
-    $scope.getSearchCust = function(vals) {
-      var searchTerms = {search: vals};
-        const httpOptions = {
-            headers: {
-              'Content-Type':  'application/json',
-              'Authorization': 'Bearer '+localStorage.getItem("unitech_admin_access_token")
-            }
-        };
-        return $http.post($rootScope.baseURL+'/customer/typeahead/search', searchTerms, httpOptions).then((result) => {
-            return result.data;
-        });
     };
 
     $scope.getSearchMachine = function(vals) {
@@ -428,14 +288,14 @@ angular.module('quotation').controller('quotationEditCtrl', function ($rootScope
                             "</table>" +
                             "<table class='table table-stripped table-bordered' style='font-size:11pt'>" +
                               "<tr>" +
-                                "<td colspan='2'>To: <strong>"+$scope.quotation.qm_dm_id.cm_name+" ("+$scope.quotation.qm_dm_id.cm_address+")</strong></td>"+
+                                "<td colspan='2'>To: <strong>"+$scope.quotation.cm_name+" ("+$scope.quotation.cm_address+")</strong></td>"+
                               "</tr>" +
                               "<tr>" +
                                 "<td>Date : <strong>"+$filter('date')($scope.quotation.qm_date,'mediumDate')+"</strong></td>" +
                                 "<td>Reference : <strong>"+$scope.quotation.qm_ref+"</strong></td>" +
                               "</tr>" +
                               "<tr>" +
-                                "<td>Design No : <strong>"+$scope.quotation.qm_dm_id.dm_design_no+"</strong></td>" +
+                                "<td>Design No : <strong>"+$scope.quotation.dm_design_no+"</strong></td>" +
                                 "<td>Quotation No : <strong>"+$scope.quotation.qm_quotation_no+"</strong></td>" +
                               "</tr>" +
                             "</table>" +
