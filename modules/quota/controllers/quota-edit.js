6adab544ed507f42a0eb3d80b7971d92d55b106b
clayton24 <dsouzaclayton95@gmail.com>
2018-12-14 17:38:16 +0530

    All Done with table - Fixed header,Column and tab key

--- modules/quota/controllers/quota-edit.js
+++ modules/quota/controllers/quota-edit.js
@@ -1368,20 +1368,23 @@ angular.module('quota').controller('quotaEditCtrl', function ($rootScope, $http,
           }
     });
 
-  $('table').on('scroll', function() {
-    $("#" + this.id + " > *").width($(this).width() + $(this).scrollLeft());
-  });
+    // tab key
+    // $("#content").keydown(function(objEvent) {
+    //     if (objEvent.keyCode == 9) {  //tab pressed
+    //         objEvent.preventDefault(); // stops its action
+    //     }
+    // })
 
-
-    // $("#content").on( 'scroll', function(){
-    //    console.log('Event Fired');
-
-    // });
-    $("#content").keydown(function(objEvent) {
-        if (objEvent.keyCode == 9) {  //tab pressed
-            objEvent.preventDefault(); // stops its action
-        }
-    })
-
-   
+    $scope.name = 'World';
+    $(document).ready(function() {
+      $('tbody').scroll(function(e) { //detect a scroll event on the tbody
+        /*
+        Setting the thead left value to the negative valule of tbody.scrollLeft will make it track the movement
+        of the tbody element. Setting an elements left value to that of the tbody.scrollLeft left makes it maintain       it's relative position at the left of the table.    
+        */
+        $('thead').css("left", -$("tbody").scrollLeft()); //fix the thead relative to the body scrolling
+        $('thead th:nth-child(1)').css("left", $("tbody").scrollLeft()); //fix the first cell of the header
+        $('tbody td:nth-child(1)').css("left", $("tbody").scrollLeft()); //fix the first column of tdbody
+      });
+    });
 });
